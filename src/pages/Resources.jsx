9da4a93e8464138ad123d956d1762071b0587de2
David Oparah <davidoparah@gmail.com>
2023-03-09 18:52:36 +0000

    added pages

---
+++ src/pages/Resources.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+export default function resources() {
+  return (
+    <>
+      <Nav />
+      <h1> Hello Resources </h1>
+    </>
+  );
+}
